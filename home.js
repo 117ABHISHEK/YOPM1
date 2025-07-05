3d4015f61b5f2a696340a2064eb52a2ea2d8e454
117ABHISHEK <147874160+117ABHISHEK@users.noreply.github.com>
2025-07-05 17:12:12 +0530

    Update home.js

--- home.js
+++ home.js
@@ -9,7 +9,7 @@ function handleKeyPress(event) {
 }
 
 function call() {
-  // let API_KEY = ""; // Replace when public 
+  const apiKey = "" // Replace when public
   const city = document.getElementById("cityInput").value.trim()
 
   if (!city) {
@@ -17,7 +17,7 @@ function call() {
     return
   }
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
   // Show loading indicator
   showLoading(true)
